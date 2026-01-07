befc493571e5ab091d00984acc02dc07a287cb55
MolnarMarcell <marci20070208@gmail.com>
2026-01-07 12:48:02 +0100

    js2

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
 let kontener = document.createElement('div');
-kontener.id = 'kontener';
+kontener.className = 'cards-grid';
 document.body.appendChild(kontener);
 
-// külön elem az űrhajósok számának
-let astronautakSzama = document.createElement('h2');
-astronautakSzama.id = 'astronauts';
-document.body.insertBefore(astronautakSzama, kontener);
+let select = document.getElementById("station-filter");
 
-function megjelenitAdatok(adatok) {
-    astronautakSzama.textContent = `Űrben tartózkodók száma: ${adatok.number}`;
+let astronautak = [];
 
-    adatok.people.forEach(person => {
+let emberek = document.getElementById("emberek");
+
+let db = 0;
+
+function megjelenitKartyak(lista) {
+    kontener.innerHTML = "";
+
+    lista.forEach(person => {
         let kartya = document.createElement('div');
-        kartya.classList.add('kartya');
+        kartya.classList.add('card');
 
         kartya.innerHTML = `
             <p><strong>Név:</strong> ${person.name}</p>
             <p><strong>Űrállomás:</strong> ${person.craft}</p>
         `;
+        db++;
+        emberek.textContent = `${db}`;
 
         kontener.appendChild(kartya);
     });
 }
 
+function feltoltAllomasok(lista) {
+    let allomasok = [...new Set(lista.map(p => p.craft))];
+
+    allomasok.forEach(allomas => {
+        let option = document.createElement("option");
+        option.value = allomas;
+        option.textContent = allomas;
+        select.appendChild(option);
+    });
+}
+
+select.addEventListener("change", function () {
+    let kivalasztott = this.value;
+
+    if (kivalasztott === "all") {
+        megjelenitKartyak(astronautak);
+    } else {
+        let szurt = astronautak.filter(p => p.craft === kivalasztott);
+        megjelenitKartyak(szurt);
+    }
+});
+
 fetch('http://api.open-notify.org/astros.json')
     .then(response => response.json())
-    .then(data => megjelenitAdatok(data))
-    .catch(error => console.error('Hiba történt az adatok lekérése során:', error));
+    .then(data => {
+        astronautak = data.people;
+        feltoltAllomasok(astronautak);
+        megjelenitKartyak(astronautak);
+    })
+    .catch(error => console.error('Hiba:', error));
+
